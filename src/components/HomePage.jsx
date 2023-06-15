8f0dc26eb22d9b62baaeb3c40a80b3045007aefc
admirerbrown <admirerbrown@gmail.com>
2023-06-15 22:03:50 +0000

    refactor hero section

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,9 @@
 const Home = () => {
   return (
-    <div className="bg-background_image  h-3/6 bg-no-repeat bg-cover ">
-      <div className="container mx-auto px-4 h-screen ">
-        <div className="h-full flex flex-col justify-center items-center">
-          <h1 className="text-white text-4xl font-bold">Hello World</h1>
-        </div>
-      </div>
+    <div className="bg-background_image bg-no-repeat bg-cover h-screen flex justify-center items-center">
+      <h1 className="text-white text-4xl font-bold flex justify-center items-center">
+        Hello World
+      </h1>
     </div>
   );
 };
