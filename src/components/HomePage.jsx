81a1101a86f54b5a98f8841c10e84cd0b1987979
admirerbrown <admirerbrown@gmail.com>
2023-06-22 19:44:49 +0000

    make hero section form glassmorphism

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -7,16 +7,22 @@ import { Button } from "flowbite-react";
 
 const Home = () => {
   return (
-    <div className="bg-background_image bg-no-repeat bg-cover h-screen flex justify-center items-center">
-      <StaticCarousel></StaticCarousel>
+    <div className="">
+      <div className="bg-background_image bg-no-repeat bg-cover h-screen flex justify-end items-center">
+        <StaticCarousel className=""></StaticCarousel>
+        <Form className=""></Form>
+      </div>
+      
+
+      
     </div>
   );
 };
 
 const StaticCarousel = () => {
   return (
-    <Carousel>
-      <div className=" flex h-full bg-sky-800 bg-opacity-50 justify-center flex-col">
+    <Carousel className="relative z-1 ">
+      <div className=" flex h-full bg-sky-800 bg-opacity-50 justify-center align-items-center flex-col p-24">
         <div className="flex gap-5 mx-4 mb-3">
           <FaFacebookF className="text-white text-2xl"></FaFacebookF>
           <TfiTwitterAlt className="text-white text-2xl"></TfiTwitterAlt>
@@ -24,17 +30,17 @@ const StaticCarousel = () => {
         </div>
 
         <h3 className=" text-white text-4xl mx-4 mb-5">Hello World</h3>
-        <p className=" text-gray-300 text-50 mx-4 mb-5">
+        <p className=" text-gray-300 text-50 mx-4 mb-5 flex justify-center align-items-center w-2/5">
           A conference is an organized event where individuals with shared
           interests or professional backgrounds come together to exchange
           knowledge, network, and discuss various topics. Conferences typically
           feature presentations, workshops, panel discussions, and interactive
           sessions led by experts in their respective fields.
         </p>
-        <DefaultButtons></DefaultButtons>
+        <DefaultButtons text="Learn More"></DefaultButtons>
       </div>
 
-      <div className=" flex h-full bg-sky-800 bg-opacity-50 justify-center flex-col">
+      <div className=" flex h-full bg-sky-800 bg-opacity-50 justify-center flex-col p-24">
         <div className="flex gap-5 mx-4 mb-3">
           <FaFacebookF className="text-white text-2xl"></FaFacebookF>
           <TfiTwitterAlt className="text-white text-2xl"></TfiTwitterAlt>
@@ -42,23 +48,66 @@ const StaticCarousel = () => {
         </div>
 
         <h3 className=" text-white text-4xl mx-4 mb-5">Hello World</h3>
-        <p className=" text-gray-300 text-50 mx-4 mb-5">
-          Conferences can cover a wide range of subjects, including academic research, 
-          technology, business, medicine, arts and culture, social issues, and more. 
+        <p className=" text-gray-300 text-50 mx-4 mb-5 w-2/5">
+          Conferences can cover a wide range of subjects, including academic
+          research, technology, business, medicine, arts and culture, social
+          issues, and more.
         </p>
-        <DefaultButtons></DefaultButtons>
+        <DefaultButtons text="Learn More"></DefaultButtons>
       </div>
     </Carousel>
   );
 };
 
-const DefaultButtons = () => {
+const DefaultButtons = ({ text }) => {
   return (
     <>
       <Button className="bg-orange-600 w-44 mx-5 " color="dark">
-        Learn more
+        {text}
       </Button>
     </>
+  );
+};
+
+const Form = () => {
+  return (
+    <div className="bg-gray-900 absolute z-50 h-3/5 p-8 rounded-lg backdrop-filter backdrop-blur-lg bg-opacity-30 m-24">
+      <div className="flex flex-col">
+        <h4 className="text-white">EVENT REGISTER</h4>
+        <hr className="border-gray-300" />
+        <form action="" className="flex flex-col">
+          <input
+            type="text"
+            maxLength={20}
+            placeholder="Email address"
+            className="bg-transparent border-2 border-gray-300 rounded-lg p-2 mb-2 text-white placeholder-gray-400 focus:outline-none focus:border-blue-500"
+            required
+          />
+          <input
+            type="text"
+            maxLength={20}
+            placeholder="Phone Number"
+            className="bg-transparent border-2 border-gray-300 rounded-lg p-2 mb-2 text-white placeholder-gray-400 focus:outline-none focus:border-blue-500"
+            required
+          />
+          <input
+            type="text"
+            maxLength={20}
+            placeholder="Quantity"
+            className="bg-transparent border-2 border-gray-300 rounded-lg p-2 mb-2 text-white placeholder-gray-400 focus:outline-none focus:border-blue-500"
+            required
+          />
+          <input
+            type="text"
+            maxLength={20}
+            placeholder="I am attending to"
+            className="bg-transparent border-2 border-gray-300 rounded-lg p-2 mb-2 text-white placeholder-gray-400 focus:outline-none focus:border-blue-500"
+            required
+          />
+        </form>
+      </div>
+      <DefaultButtons text="REGISTER ME" className="mt-4" />
+    </div>
   );
 };
 
