0c10704d29961f03a11877d8fccbe2f812d7b611
admirerbrown <admirerbrown@gmail.com>
2023-06-24 00:08:45 +0000

    style navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,11 +6,11 @@ export default function NavMenu() {
   return (
     <Navbar fluid >
       <Navbar.Brand href="https://flowbite-react.com">
-        {/* <img
-          alt="Flowbite React Logo"
+        <img
+          alt="company Logo"
           className="mr-3 h-6 sm:h-9"
-          src="/favicon.svg"
-        /> */}
+          src="/apple-touch-icon.png"
+        />
         <span className="self-center whitespace-nowrap text-xl font-semibold text-white">
           <h2>Alum-nite</h2>
         </span>
