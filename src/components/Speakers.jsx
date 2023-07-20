e7aa3aa231f76851cec3e0f93f3879a38d7c1fb7
admirerbrown <admirerbrown@gmail.com>
2023-07-20 14:32:06 +0000

    add carousel speaker description

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -1,30 +1,87 @@
 import divider from "../assets/images/divider.png";
 import React, { useState } from "react";
 
-
 const Speakers = () => {
- 
-const images = [
-  {
-    src: 'https://picsum.photos/id/1018/1000/600/',
-  },
-  {
-    src: 'https://picsum.photos/id/1015/1000/600/',
-  },
-  {
-    src: 'https://picsum.photos/id/1019/1000/600/',
-  },
-  {
-    src: 'https://picsum.photos/id/1018/1000/600/',
-  },
-  {
-    src: 'https://picsum.photos/id/1015/1000/600/',
-  },
-  {
-    src: 'https://picsum.photos/id/1019/1000/600/',
-  },
-
-];
+  const speakers = [
+    {
+      src: "https://picsum.photos/id/1018/1000/600/",
+      name: "Mercy Lavie",
+      title: "Product Manager",
+      summary: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore animi 
+      sequi velit nihil officiis aspernatur, necessitatibus rerum illo aliquid aliquam?
+       Ullam ipsam veniam ratione asperiores ipsum excepturi vitae sit necessitatibus.`,
+      roles: [
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+      ],
+    },
+    {
+      src: "https://picsum.photos/id/1015/1000/600/",
+      name: "Samuel Donovan",
+      title: "Product Manager",
+      summary: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore animi 
+      sequi velit nihil officiis aspernatur, necessitatibus rerum illo aliquid aliquam?
+       Ullam ipsam veniam ratione asperiores ipsum excepturi vitae sit necessitatibus.`,
+      roles: [
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+      ],
+    },
+    {
+      src: "https://picsum.photos/id/1019/1000/600/",
+      name: "Mercy Lavie",
+      title: "Snr. Recruiting Officer",
+      summary: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore animi 
+      sequi velit nihil officiis aspernatur, necessitatibus rerum illo aliquid aliquam?
+       Ullam ipsam veniam ratione asperiores ipsum excepturi vitae sit necessitatibus.`,
+      roles: [
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+      ],
+    },
+    {
+      src: "https://picsum.photos/id/1018/1000/600/",
+      name: "ELANA DORAN",
+      title: "Co.Founder",
+      summary: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore animi 
+      sequi velit nihil officiis aspernatur, necessitatibus rerum illo aliquid aliquam?
+       Ullam ipsam veniam ratione asperiores ipsum excepturi vitae sit necessitatibus.`,
+      roles: [
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+      ],
+    },
+    {
+      src: "https://picsum.photos/id/1015/1000/600/",
+      name: "JESSICA PALT",
+      title: "Founder",
+      summary: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore animi 
+      sequi velit nihil officiis aspernatur, necessitatibus rerum illo aliquid aliquam?
+       Ullam ipsam veniam ratione asperiores ipsum excepturi vitae sit necessitatibus.`,
+      roles: [
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+      ],
+    },
+    {
+      src: "https://picsum.photos/id/1019/1000/600/",
+      name: "Mavis Beacon",
+      title: "Designer",
+      summary: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore animi 
+      sequi velit nihil officiis aspernatur, necessitatibus rerum illo aliquid aliquam?
+       Ullam ipsam veniam ratione asperiores ipsum excepturi vitae sit necessitatibus.`,
+      roles: [
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+        "Lorem ipsum dolor sit amet consectetur",
+      ],
+    },
+  ];
 
   return (
     <>
@@ -39,8 +96,8 @@ const images = [
         <img src={divider} alt="divider" className="w-20 mt-5" />
       </div>
       <div>
-        <div className="carousel w-1/2">
-          <Carousel images={images} />
+        <div className="">
+          <Carousel speakers={speakers} />
         </div>
         <div className="speaker_description"></div>
       </div>
@@ -48,8 +105,7 @@ const images = [
   );
 };
 
-
-const Carousel = ({ images }) => {
+const Carousel = ({ speakers }) => {
   const [activeSlide, setActiveSlide] = useState(0);
 
   const changeSlide = (index) => {
@@ -57,36 +113,57 @@ const Carousel = ({ images }) => {
   };
 
   return (
-    <div className="relative">
-      <div className="carousel">
-        {images.map((image, index) => (
+    <div className="lg:flex">
+      <div className="carousel lg:w-1/2 relative">
+        <div className="carousel">
+          {speakers.map((speaker, index) => (
+            <div
+              key={index}
+              className={`slide ${
+                index === activeSlide ? "block" : "hidden"
+              } w-full h-full`}
+            >
+              <img
+                src={speaker.src}
+                alt={`${index + 1}`}
+                className="object-cover w-full h-full"
+              />
+            </div>
+          ))}
+        </div>
+
+        <div className="thumbnails absolute bottom-0 left-0 flex justify-center w-full mb-2 p-3 bg-neutral-500 opacity-70">
+          {speakers.map((speaker, index) => (
+            <div
+              key={index}
+              className="cursor-pointer  border-4 mx-1 border-transparent relative hover:border-[#e45914]"
+              onClick={() => changeSlide(index)}
+            >
+              <img
+                src={speaker.src}
+                alt={`${index + 1}`}
+                className="w-36 h-24 object-cover"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="description lg:w-1/2 bg-[#242732] text-white">
+        {speakers.map((speaker, index) => (
           <div
-            key={index}
             className={`slide ${
-              index === activeSlide ? 'block' : 'hidden'
+              index === activeSlide ? "block" : "hidden"
             } w-full h-full`}
           >
-            <img
-              src={image.src}
-              alt={`${index + 1}`}
-              className="object-cover w-full h-full"
-            />
-          </div>
-        ))}
-      </div>
-
-      <div className="thumbnails absolute bottom-0 left-0 flex justify-center w-full mb-2 p-3 bg-neutral-500 opacity-70">
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className="cursor-pointer  border-4 mx-1 border-transparent relative hover:border-[#e45914]"
-            onClick={() => changeSlide(index)}
-          >
-            <img
-              src={image.src}
-              alt={`${index + 1}`}
-              className="w-36 h-24 object-cover"
-            />
+            <h1 className="text-3xl mb-2">{speaker.name.toUpperCase()}</h1>
+            <p>{speaker.title.toUpperCase()}</p>
+            <hr />
+            <p>{speaker.summary}</p>
+            <ul>
+              {speaker.roles.map((role, index) => (
+                <li>{role}</li>
+              ))}
+            </ul>
           </div>
         ))}
       </div>
