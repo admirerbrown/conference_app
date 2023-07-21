59a763e3fb3e68f4c3bd7f99a5f29bfc81fae88b
admirerbrown <admirerbrown@gmail.com>
2023-07-21 16:07:55 +0000

    only uploading image left for speakers section

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -1,5 +1,12 @@
 import divider from "../assets/images/divider.png";
 import React, { useState } from "react";
+import { HiBadgeCheck } from "react-icons/hi";
+import { AiOutlineLine } from "react-icons/ai";
+import { RiFacebookLine } from "react-icons/ri";
+
+import { AiOutlineTwitter } from "react-icons/ai";
+import { IoLogoGoogleplus } from "react-icons/io";
+import { FaInstagram, FaLinkedinIn } from "react-icons/fa";
 
 const Speakers = () => {
   const speakers = [
@@ -7,78 +14,96 @@ const Speakers = () => {
       src: "https://picsum.photos/id/1018/1000/600/",
       name: "Mercy Lavie",
       title: "Product Manager",
-      summary: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore animi 
-      sequi velit nihil officiis aspernatur, necessitatibus rerum illo aliquid aliquam?
-       Ullam ipsam veniam ratione asperiores ipsum excepturi vitae sit necessitatibus.`,
+      summary: `Para obtener una descripción general rápida de estas frases básicas en español, 
+      consulte la hoja de trucos a continuación. Considere esto como su propio libro de frases 
+      en español visual. Pero asegúrese de leer el resto de la publicación para obtener detalles 
+      sobre la pronunciación de estas frases en español, incluido el audio de las palabras y los 
+      sonidos complicados.Y uno o dos extras de información adicional sobre el idioma y la 
+      cultura española.`,
       roles: [
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
+       "pronunciación de estas frases en español",
+        "general rápida de estas frases básicas",
+        "Y uno o dos extras de información adicional",
       ],
     },
     {
       src: "https://picsum.photos/id/1015/1000/600/",
       name: "Samuel Donovan",
       title: "Product Manager",
-      summary: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore animi 
-      sequi velit nihil officiis aspernatur, necessitatibus rerum illo aliquid aliquam?
-       Ullam ipsam veniam ratione asperiores ipsum excepturi vitae sit necessitatibus.`,
+      summary: `Para obtener una descripción general rápida de estas frases básicas en español, 
+      consulte la hoja de trucos a continuación. Considere esto como su propio libro de frases 
+      en español visual. Pero asegúrese de leer el resto de la publicación para obtener detalles 
+      sobre la pronunciación de estas frases en español, incluido el audio de las palabras y los 
+      sonidos complicados.Y uno o dos extras de información adicional sobre el idioma y la 
+      cultura española.`,
       roles: [
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
+        "pronunciación de estas frases en español",
+        "general rápida de estas frases básicas",
+        "Y uno o dos extras de información adicional",
       ],
     },
     {
       src: "https://picsum.photos/id/1019/1000/600/",
       name: "Mercy Lavie",
       title: "Snr. Recruiting Officer",
-      summary: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore animi 
-      sequi velit nihil officiis aspernatur, necessitatibus rerum illo aliquid aliquam?
-       Ullam ipsam veniam ratione asperiores ipsum excepturi vitae sit necessitatibus.`,
+      summary: `Para obtener una descripción general rápida de estas frases básicas en español, 
+      consulte la hoja de trucos a continuación. Considere esto como su propio libro de frases 
+      en español visual. Pero asegúrese de leer el resto de la publicación para obtener detalles 
+      sobre la pronunciación de estas frases en español, incluido el audio de las palabras y los 
+      sonidos complicados.Y uno o dos extras de información adicional sobre el idioma y la 
+      cultura española.`,
       roles: [
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
+        "pronunciación de estas frases en español",
+        "general rápida de estas frases básicas",
+        "Y uno o dos extras de información adicional",
       ],
     },
     {
       src: "https://picsum.photos/id/1018/1000/600/",
       name: "ELANA DORAN",
       title: "Co.Founder",
-      summary: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore animi 
-      sequi velit nihil officiis aspernatur, necessitatibus rerum illo aliquid aliquam?
-       Ullam ipsam veniam ratione asperiores ipsum excepturi vitae sit necessitatibus.`,
+      summary: `Para obtener una descripción general rápida de estas frases básicas en español, 
+      consulte la hoja de trucos a continuación. Considere esto como su propio libro de frases 
+      en español visual. Pero asegúrese de leer el resto de la publicación para obtener detalles 
+      sobre la pronunciación de estas frases en español, incluido el audio de las palabras y los 
+      sonidos complicados.Y uno o dos extras de información adicional sobre el idioma y la 
+      cultura española.`,
       roles: [
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
+        "pronunciación de estas frases en español",
+        "general rápida de estas frases básicas",
+        "Y uno o dos extras de información adicional",
       ],
     },
     {
       src: "https://picsum.photos/id/1015/1000/600/",
       name: "JESSICA PALT",
       title: "Founder",
-      summary: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore animi 
-      sequi velit nihil officiis aspernatur, necessitatibus rerum illo aliquid aliquam?
-       Ullam ipsam veniam ratione asperiores ipsum excepturi vitae sit necessitatibus.`,
+      summary: `Para obtener una descripción general rápida de estas frases básicas en español, 
+      consulte la hoja de trucos a continuación. Considere esto como su propio libro de frases 
+      en español visual. Pero asegúrese de leer el resto de la publicación para obtener detalles 
+      sobre la pronunciación de estas frases en español, incluido el audio de las palabras y los 
+      sonidos complicados.Y uno o dos extras de información adicional sobre el idioma y la 
+      cultura española.`,
       roles: [
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
+        "pronunciación de estas frases en español",
+        "general rápida de estas frases básicas",
+        "Y uno o dos extras de información adicional",
       ],
     },
     {
       src: "https://picsum.photos/id/1019/1000/600/",
       name: "Mavis Beacon",
       title: "Designer",
-      summary: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore animi 
-      sequi velit nihil officiis aspernatur, necessitatibus rerum illo aliquid aliquam?
-       Ullam ipsam veniam ratione asperiores ipsum excepturi vitae sit necessitatibus.`,
+      summary: `Para obtener una descripción general rápida de estas frases básicas en español, 
+      consulte la hoja de trucos a continuación. Considere esto como su propio libro de frases 
+      en español visual. Pero asegúrese de leer el resto de la publicación para obtener detalles 
+      sobre la pronunciación de estas frases en español, incluido el audio de las palabras y los 
+      sonidos complicados.Y uno o dos extras de información adicional sobre el idioma y la 
+      cultura española.`,
       roles: [
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
-        "Lorem ipsum dolor sit amet consectetur",
+        "pronunciación de estas frases en español",
+        "general rápida de estas frases básicas",
+        "Y uno o dos extras de información adicional",
       ],
     },
   ];
@@ -95,11 +120,9 @@ const Speakers = () => {
         </p>
         <img src={divider} alt="divider" className="w-20 mt-5" />
       </div>
-      <div>
-        <div className="">
-          <Carousel speakers={speakers} />
-        </div>
-        <div className="speaker_description"></div>
+
+      <div className="">
+        <Carousel speakers={speakers} />
       </div>
     </>
   );
@@ -113,7 +136,7 @@ const Carousel = ({ speakers }) => {
   };
 
   return (
-    <div className="lg:flex">
+    <div className="lg:flex bg-[#242732]">
       <div className="carousel lg:w-1/2 relative">
         <div className="carousel">
           {speakers.map((speaker, index) => (
@@ -132,7 +155,7 @@ const Carousel = ({ speakers }) => {
           ))}
         </div>
 
-        <div className="thumbnails absolute bottom-0 left-0 flex justify-center w-full mb-2 p-3 bg-neutral-500 opacity-70">
+        <div className="thumbnails absolute bottom-0 left-0 flex justify-center w-full p-3 bg-neutral-500 opacity-70">
           {speakers.map((speaker, index) => (
             <div
               key={index}
@@ -142,30 +165,58 @@ const Carousel = ({ speakers }) => {
               <img
                 src={speaker.src}
                 alt={`${index + 1}`}
-                className="w-36 h-24 object-cover"
+                className="w-36 h-16 lg:h-24 object-cover"
               />
             </div>
           ))}
         </div>
       </div>
-      <div className="description lg:w-1/2 bg-[#242732] text-white">
-        {speakers.map((speaker, index) => (
-          <div
-            className={`slide ${
-              index === activeSlide ? "block" : "hidden"
-            } w-full h-full`}
-          >
-            <h1 className="text-3xl mb-2">{speaker.name.toUpperCase()}</h1>
-            <p>{speaker.title.toUpperCase()}</p>
-            <hr />
-            <p>{speaker.summary}</p>
-            <ul>
-              {speaker.roles.map((role, index) => (
-                <li>{role}</li>
-              ))}
-            </ul>
+      <div className="description lg:w-1/2 pt10 p-10">
+        <div className="flex 2xl:items-center">
+          {speakers.map((speaker, index) => (
+            <div
+              className={`slide ${
+                index === activeSlide ? "block" : "hidden"
+              } w-full h-full`}
+            >
+              <h1 className="text-3xl mb-2  text-white">
+                {speaker.name.toUpperCase()}
+              </h1>
+              <p className=" text-gray-300 ">{speaker.title.toUpperCase()}</p>
+              <AiOutlineLine className="text-4xl text-[#e45914]" />
+              <p className="mt-5 mb-9  text-gray-300 leading-7 lg:w-4/5 2xl:w-3/5">
+                {speaker.summary}
+              </p>
+              <ul className=" text-gray-300 flex flex-col gap-3">
+                {speaker.roles.map((role, index) => (
+                  <li className="flex gap-2">
+                    <HiBadgeCheck className="text-[#e45914] mt-1"></HiBadgeCheck>
+                    {role}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+
+        <div className="flex gap-2 mt-7">
+          <div className=" flex rounded-full border outline-gray-50 h-9 w-9 justify-center items-center">
+            <RiFacebookLine className="text-white"></RiFacebookLine>
           </div>
-        ))}
+          <div className=" flex rounded-full border outline-gray-50 h-9 w-9 justify-center items-center">
+            <AiOutlineTwitter className="text-white"></AiOutlineTwitter>
+          </div>
+
+          <div className=" flex rounded-full border outline-gray-50 h-9 w-9 justify-center items-center">
+            <IoLogoGoogleplus className="text-white"></IoLogoGoogleplus>
+          </div>
+          <div className=" flex rounded-full border outline-gray-50 h-9 w-9 justify-center items-center">
+            <FaInstagram className="text-white"></FaInstagram>
+          </div>
+          <div className=" flex rounded-full border outline-gray-50 h-9 w-9 justify-center items-center">
+            <FaLinkedinIn className="text-white"></FaLinkedinIn>
+          </div>
+        </div>
       </div>
     </div>
   );
